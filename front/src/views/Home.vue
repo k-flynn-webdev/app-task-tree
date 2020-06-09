d1df95bc1f92410b7e1635c73537a8dba163111d
kevin flynn <flynny85@gmail.com>
2020-06-09 03:19:53 +0100

    bugfix

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -14,7 +14,7 @@
 
 <!--    <div class="task__timer"></div>-->
 
-<!--    <TaskItems />-->
+    <TaskItems />
 
   </div>
 </template>
@@ -23,29 +23,21 @@
 // import User from '../components/User.vue'
 import Project from '../components/Project.vue'
 import TaskInput from '../components/TaskInput.vue'
-// import TaskItems from '../components/TaskItems.vue'
-// import MenuRowOpt from '../components/MenuRowOptSlide.vue'
+import TaskItems from '../components/TaskItems.vue'
 
 export default {
   name: 'Home',
   components: {
     // User,
     Project,
-    TaskInput
-    // TaskItems
-    // MenuRowOpt
+    TaskInput,
+    TaskItems
   },
-
   data () {
     return {
-      // testData: [false, false, false]
     }
   },
-
   methods: {
-    // toggleRowData: function (row) {
-    //   this.testData.splice(row, 1, !this.testData[row])
-    // }
   }
 }
 </script>
