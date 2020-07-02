b8d464513c544ad65e02104c0d0ce7dc84d86279
kevin flynn <flynny85@gmail.com>
2020-07-02 12:37:10 +0100

    test tweak

--- server/test/user.test.js
+++ server/test/user.test.js
@@ -6,30 +6,36 @@ const chai = require('chai')
 chai.use(chaiHttp)
 
 
-describe('User anon', () => {
+describe('User', () => {
 
-  it('Should get a valid account response', (done) => {
+  it('Should create a user account', (done) => {
     // todo
-    // chai.request(config.ip + ':' + config.port)
-    // .post(constants.paths.API_USER_ANON)
-    // .send({})
-    // .end(function(err, res){
-    //   expect(res).toBeDefined()
-    //   expect(res.status).toBe(201)
-    //   expect(res.body).toBeDefined()
-    //   expect(res.body.data).toBeDefined()
-    //   expect(res.body.data.account).toBeDefined()
-    //   expect(res.body.data.account.id).toBeGreaterThan(0)
-    //   expect(res.body.data.account.name).toBeDefined()
-    //   expect(res.body.data.account.role).toBeDefined()
-    //   expect(typeof res.body.data.account.name === 'string').toBe(true)
-    //   expect(typeof res.body.data.account.role === 'string').toBe(true)
-    //   expect(res.body.data.token).toBeDefined()
-    //   expect(typeof res.body.data.token === 'string').toBe(true)
-    //   expect(res.body.message).toBeDefined()
-    //   expect(res.body.message).toEqual(constants.messages.SUCCESS_CREATED_ACCOUNT)
+    chai.request(config.ip + ':' + config.port)
+    .post(constants.paths.API_USER)
+    .send({
+      name: 'testName',
+      email: 'email.user@temp.co.uk',
+      password: 'test123FOUR'
+    })
+    .end(function(err, res){
+      console.log(res.body)
+      expect(res).toBeDefined()
+      expect(res.status).toBe(201)
+      expect(res.body).toBeDefined()
+      expect(res.body.data).toBeDefined()
+      expect(res.body.data.account).toBeDefined()
+      expect(res.body.data.account.id).toBeGreaterThan(0)
+      expect(res.body.data.account.name).toBeDefined()
+      expect(res.body.data.account.role).toBeDefined()
+      expect(typeof res.body.data.account.name === 'string').toBe(true)
+      expect(typeof res.body.data.account.role === 'string').toBe(true)
+      expect(res.body.data.token).toBeDefined()
+      expect(typeof res.body.data.token === 'string').toBe(true)
+      expect(res.body.data.token.length).toBeGreaterThan(10)
+      expect(res.body.message).toBeDefined()
+      expect(res.body.message).toEqual(constants.messages.SUCCESS_CREATED_ACCOUNT)
       done()
-    // })
+    })
   })
 
 })
