b0d2b9cecf4850821b0b58d177ab019104ca5b36
kevin flynn <flynny85@gmail.com>
2020-07-25 03:48:02 +0100

    latest ui build

---
+++ front/src/components/GetUser.vue
@@ -0,0 +1,57 @@
+<template>
+  <br>
+</template>
+
+<script>
+import paths from '../constants/paths'
+import modes from '../constants/modes.js'
+import helpers from '../services/Helpers'
+import general from '../constants/general'
+import status from '../constants/status.js'
+import userService from '../services/UserService'
+
+export default {
+  name: 'GetUser',
+  props: {
+    user: {
+      type: String,
+      default: modes.CLEAR
+    }
+  },
+  mounted () {
+    return this.getUser()
+  },
+  methods: {
+    getUser: function () {
+      return userService.getUserDev({ user: this.user })
+        .then(res => this.handleSuccess(res))
+        .catch(err => this.handleError(err))
+    },
+    handleSuccess: function (res) {
+      this.$store.commit('status', status.SUCCESS)
+      const toastMsg = {
+        message: res.data.message,
+        isTimed: true,
+        isError: false
+      }
+      this.$store.commit('toasts/toastAdd', toastMsg)
+
+      helpers.timeDelay(() => {
+        this.$router.push({ name: paths.HOME })
+      }, general.DELAY)
+
+      helpers.timeDelay(() => {
+        this.$store.commit('status', status.CLEAR)
+      }, general.DELAY_SUCCESS + general.DELAY)
+    },
+    handleError: function (err) {
+      this.$store.commit('status', status.ERROR)
+      this.$store.commit('toasts/toastAdd', err)
+
+      helpers.timeDelay(() => {
+        this.$store.commit('status', status.CLEAR)
+      }, general.DELAY_ERROR)
+    }
+  }
+}
+</script>
