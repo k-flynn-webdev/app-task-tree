25e6bab9f00a66e79456e443a99b029734245c7c
kevin flynn <flynny85@gmail.com>
2020-08-31 17:00:38 +0100

    Add new payload to Auth service

--- server2/src/authentication.js
+++ server2/src/authentication.js
@@ -2,8 +2,28 @@ const { AuthenticationService, JWTStrategy } = require('@feathersjs/authenticati
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
 
+/**
+ * Extend the standard AuthenticationService package to include
+ *      additional data from the User object.
+ */
+class ExtendAuthPayload extends AuthenticationService {
+  async getPayload(authResult, params) {
+    // Call original `getPayload` first
+    const payload = await super.getPayload(authResult, params);
+    const { user } = authResult;
+
+    if (user) {
+      if (user.id) payload.id = user.id;
+      if (user.role) payload.role = user.role;
+      if (user.created_at) payload.created_at = user.created_at;
+    }
+
+    return payload;
+  }
+}
+
 module.exports = app => {
-  const authentication = new AuthenticationService(app);
+  const authentication = new ExtendAuthPayload(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
