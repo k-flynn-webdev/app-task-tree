7f771550540de8054c48e24406ba7ba7a1317082
kevin flynn <flynny85@gmail.com>
2020-10-01 20:04:36 +0100

    Add page mixin

--- front2/src/views/Projects.vue
+++ front2/src/views/Projects.vue
@@ -19,8 +19,6 @@
 <script>
 import rowCreate from '../components/rowCreate'
 import rowItem from '../components/rowItem'
-import { TYPES } from '../constants'
-import { get } from 'lodash-es'
 import pagesMixin from '../mixins/Pages'
 
 export default {
@@ -44,79 +42,7 @@ export default {
       type: String,
       default: ''
     }
-  },
-
-  // computed: Items.computed,
-  // mounted: Items.mounted().bind(this),
-  // methods: Items.methods,
-
-  // computed: {
-  //   current () {
-  //     return this.$store.state[TYPES[this.type].store].current || { id: -1 }
-  //   },
-  //   items () {
-  //     return this.$store.state[TYPES[this.type].store].items
-  //   },
-  //   mode () {
-  //     return TYPES[this.type]
-  //   }
-  // },
-
-  // mounted () {
-  //   this.$store.commit('mode', TYPES[this.type])
-  //   this.$store.commit('setQuery', this.$route.query)
-  //
-  //   return this.get()
-  //   .then (() => {
-  //     const queryParam = Object.entries(this.$route.query)
-  //     if (queryParam.length) {
-  //       const keyType = queryParam[0][0]
-  //       this.$store.dispatch(`${TYPES[keyType].store}/getById`,
-  //           { id: queryParam[0][1] })
-  //       .then(({ data }) => {
-  //         this.$store.commit('setOpened', data.data)
-  //       })
-  //     }
-  //   })
-  // },
-
-  // methods: {
-  //   /**
-  //    * Get Items via API
-  //    *
-  //    * @return {Promise}
-  //    */
-  //   get () {
-  //     if (this.isLoading) return
-  //
-  //     this.isLoading = true
-  //
-  //     return this.$store.dispatch(`${TYPES[this.type].store}/get`,
-  //         { query: this.$route.query })
-  //       .then(res => {
-  //         this.isLoading = false
-  //
-  //         this.$buefy.toast.open({
-  //           duration: 1500,
-  //           message: get(res, 'data.message', 'success'),
-  //           position: 'is-top',
-  //           type: 'is-success'
-  //         })
-  //       })
-  //       .catch(err => {
-  //         this.isLoading = false
-  //
-  //         this.$buefy.toast.open({
-  //           duration: 5000,
-  //           message: get(err, 'response.data.message', 'error'),
-  //           position: 'is-top',
-  //           type: 'is-danger'
-  //         })
-  //
-  //         throw err
-  //       })
-  //   }
-  // }
+  }
 }
 </script>
 
--- front2/src/views/Tasks.vue
+++ front2/src/views/Tasks.vue
@@ -4,12 +4,12 @@
     <div class="columns is-centered flex-wrap">
       <row-create :type="type" />
 
-      <row-item v-for="item in items"
+      <row-item v-for="item in pageItems"
                 :key="item.id"
                 :item="item"
                 :type="type"
-                :class="{ 'is-active': current.id === item.id }"
-                @reload="get"
+                :class="{ 'is-active': pageCurrentItem.id === item.id }"
+                @reload="getPageItems"
       />
     </div>
 
@@ -19,11 +19,12 @@
 <script>
 import rowCreate from '../components/rowCreate'
 import rowItem from '../components/rowItem'
-import { TYPES } from '../constants'
-import { get } from 'lodash-es'
+import pagesMixin from '../mixins/Pages'
 
 export default {
-  name: 'Tasks',
+  name: 'Projects',
+
+  mixins: [pagesMixin],
 
   components: {
     rowItem,
@@ -40,74 +41,6 @@ export default {
     type: {
       type: String,
       default: ''
-    }
-  },
-
-  computed: {
-    current () {
-      return this.$store.state[TYPES[this.type].store].current || { id: -1 }
-    },
-    items () {
-      return this.$store.state[TYPES[this.type].store].items
-    },
-    mode () {
-      return TYPES[this.type]
-    }
-  },
-
-  mounted () {
-    this.$store.commit('mode', TYPES[this.type])
-    this.$store.commit('setQuery', this.$route.query)
-
-    return this.get()
-    .then (() => {
-      const queryParam = Object.entries(this.$route.query)
-      if (queryParam.length) {
-        const keyType = queryParam[0][0]
-        this.$store.dispatch(`${TYPES[keyType].store}/getById`,
-            { id: queryParam[0][1] })
-        .then(({ data }) => {
-          this.$store.commit('setOpened', data.data)
-        })
-      }
-    })
-  },
-
-  methods: {
-    /**
-     * Get Items via API
-     *
-     * @return {Promise}
-     */
-    get () {
-      if (this.isLoading) return
-
-      this.isLoading = true
-
-      return this.$store.dispatch(`${TYPES[this.type].store}/get`,
-          { query: this.$route.query })
-      .then(res => {
-        this.isLoading = false
-
-        this.$buefy.toast.open({
-          duration: 1500,
-          message: get(res, 'data.message', 'success'),
-          position: 'is-top',
-          type: 'is-success'
-        })
-      })
-      .catch(err => {
-        this.isLoading = false
-
-        this.$buefy.toast.open({
-          duration: 5000,
-          message: get(err, 'response.data.message', 'error'),
-          position: 'is-top',
-          type: 'is-danger'
-        })
-
-        throw err
-      })
     }
   }
 }
