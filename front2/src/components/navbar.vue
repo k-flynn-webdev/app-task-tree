9439b2f80263502656abaf7bbb533e9af7d01862
kevin flynn <flynny85@gmail.com>
2020-10-14 02:07:08 +0100

    updated styles

--- front2/src/components/navbar.vue
+++ front2/src/components/navbar.vue
@@ -7,16 +7,19 @@
             @update:active="closeNavBar">
     <template slot="brand">
 
-      <svg class="app-icon icon fill-primary" viewBox="0 0 100 100" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
-        <path d="M76.37,36.698c0,-9.812 -7.966,-17.778 -17.778,-17.778l-35.555,0c-9.812,0 -17.778,7.966 -17.778,17.778l-0,35.556c-0,9.812 7.966,17.778 17.778,17.778l35.555,-0c9.812,-0 17.778,-7.966 17.778,-17.778l0,-35.556Z" style="fill: rgba(0, 0, 0, 0.2);"/>
-        <path d="M43.492,84.926l-35.66,-35.661l18.228,-18.229l17.369,17.37l35.538,-35.538l18.292,18.291l-53.767,53.767Z" />
-      </svg>
+      <b-navbar-item
+          tag="router-link"
+          :to="home.route">
+        <svg class="app-icon icon fill-primary" viewBox="0 0 100 100" version="1.1" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+          <path d="M76.37,36.698c0,-9.812 -7.966,-17.778 -17.778,-17.778l-35.555,0c-9.812,0 -17.778,7.966 -17.778,17.778l-0,35.556c-0,9.812 7.966,17.778 17.778,17.778l35.555,-0c9.812,-0 17.778,-7.966 17.778,-17.778l0,-35.556Z" style="fill: rgba(0, 0, 0, 0.2);"/>
+          <path d="M43.492,84.926l-35.66,-35.661l18.228,-18.229l17.369,17.37l35.538,-35.538l18.292,18.291l-53.767,53.767Z" />
+        </svg>
+      </b-navbar-item>
 
       <b-navbar-item
           tag="router-link"
           class="navbar__main"
           :to="home.route">
-
         <h1 class="is-size-4 navbar__main-title">
           <strong>
             {{ title }}
